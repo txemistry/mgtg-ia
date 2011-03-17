22c74dbdaf4f1ec47b908dfda8241f6fc0406fa4
jose.gg88 <jose.gg88@6c77d695-def2-3e66-23b7-90933a5c719b>
2011-03-17 14:15:47 +0000

    arreglado primer  conflicto con svn... habra mas

--- trunk/AIKE-19/src/es/deusto/ingenieria/aike/maze/PuzzleProblem.java
+++ trunk/AIKE-19/src/es/deusto/ingenieria/aike/maze/PuzzleProblem.java
@@ -8,34 +8,41 @@ import java.util.List;
 import es.deusto.ingenieria.aike.formulation.Operator;
 import es.deusto.ingenieria.aike.formulation.Problem;
 import es.deusto.ingenieria.aike.formulation.State;
-import es.deusto.ingenieria.aike.puzzle.CarMovement.Destination;
+import es.deusto.ingenieria.aike.maze.CarMovement.Destination;
 import es.deusto.ingenieria.ingenieria.search.Node;
 import es.deusto.ingenieria.ingenieria.search.SearchMethod;
 
-public class PuzzleProblem extends Problem {
+public class MazeProblem extends Problem 
+{
 	
-	public PuzzleProblem(){
+	public MazeProblem()
+	{
 		super();
-		this.crearEstadosFinales();
 		this.createOperators();
 	}
 
-	private void crearEstadosFinales() {
-		this.addFinalState(new State(new Board(0,1,2,3,4,5,6,7,8)));
-	}
 
-	public void createOperators() {
+	public void createOperators() 
+	{
 		Operator operator = new CarMovement(Destination.UP);
 		this.addOperator(operator);
+		System.out.println("Se ha creado el operador UP");
+		
 		operator = new CarMovement(Destination.DOWN);
 		this.addOperator(operator);
+		System.out.println("Se ha creado el operador DOWN");
+		
 		operator = new CarMovement(Destination.RIGHT);
 		this.addOperator(operator);
+		System.out.println("Se ha creado el operador RIGHT");
+		
 		operator = new CarMovement(Destination.LEFT);
 		this.addOperator(operator);
+		System.out.println("Se ha creado el operador LEFT");
 	}
 	
-	public void solve(SearchMethod searchMethod) {
+	public void solve(SearchMethod searchMethod) 
+	{
 		SimpleDateFormat formatter = new SimpleDateFormat("hh:mm:ss");
 		System.out.println("\n* Begin '" + searchMethod.getClass().getCanonicalName() + "' (" + formatter.format(GregorianCalendar.getInstance().getTime()) + ")");
 		Node finalNode = searchMethod.search(this, this.getInitialStates().get(0));
@@ -49,5 +56,18 @@ public class PuzzleProblem extends Problem {
 		} else {
 			System.out.println("* Solution not found :-(");
 		}
+	}
+	
+	public boolean isFinalState(State state)
+	{
+		//cuando un estado es final?
+		//cuando la tile en la que esta el coche es de tipo F
+		
+		Board board = (Board)state.getInformation();
+		Tile currentTile= board.getCar();
+		
+		if(currentTile.getType().equals("F"))
+			return true;
+		else return false;
 	}
 }
